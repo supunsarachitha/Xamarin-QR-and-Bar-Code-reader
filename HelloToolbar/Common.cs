23135c06f0062dd67cd93de6dd69b3196d73e7a6
Supun Sarachitha Liyanaarachchi <36870488+supunsarachitha@users.noreply.github.com>
2018-07-14 23:36:34 +0530

    2018/0714
    2018/07/14

---
+++ HelloToolbar/Common.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PandaQRCodeReader
+{
+    public class Common
+    {
+        public static List<string> HistoryItem { get; internal set; }
+        public static string DirectoryPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "stechbuzz.db3");
+
+    }
+}
---
+++ HelloToolbar/obj/Debug/android/src/md57c9ef922ab87b6b70afc04c0438a2c22/CameraEventsListener.java
@@ -0,0 +1,58 @@
+package md57c9ef922ab87b6b70afc04c0438a2c22;
+
+
+public class CameraEventsListener
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer,
+		android.hardware.Camera.PreviewCallback,
+		android.hardware.Camera.AutoFocusCallback
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onPreviewFrame:([BLandroid/hardware/Camera;)V:GetOnPreviewFrame_arrayBLandroid_hardware_Camera_Handler:ApxLabs.FastAndroidCamera.INonMarshalingPreviewCallbackInvoker, FastAndroidCamera, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onAutoFocus:(ZLandroid/hardware/Camera;)V:GetOnAutoFocus_ZLandroid_hardware_Camera_Handler:Android.Hardware.Camera/IAutoFocusCallbackInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("ZXing.Mobile.CameraAccess.CameraEventsListener, ZXingNetMobile, Version=2.4.1.0, Culture=neutral, PublicKeyToken=null", CameraEventsListener.class, __md_methods);
+	}
+
+
+	public CameraEventsListener ()
+	{
+		super ();
+		if (getClass () == CameraEventsListener.class)
+			mono.android.TypeManager.Activate ("ZXing.Mobile.CameraAccess.CameraEventsListener, ZXingNetMobile, Version=2.4.1.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onPreviewFrame (byte[] p0, android.hardware.Camera p1)
+	{
+		n_onPreviewFrame (p0, p1);
+	}
+
+	private native void n_onPreviewFrame (byte[] p0, android.hardware.Camera p1);
+
+
+	public void onAutoFocus (boolean p0, android.hardware.Camera p1)
+	{
+		n_onAutoFocus (p0, p1);
+	}
+
+	private native void n_onAutoFocus (boolean p0, android.hardware.Camera p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
